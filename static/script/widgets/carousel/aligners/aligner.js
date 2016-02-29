fa67e3232d00e43a1dbd7b6fdb757674956b3bd9
Luke Preston <luke.preston@bbc.co.uk>
2016-02-29 16:10:48 +0000

    testing the aligner

--- static/script/widgets/carousel/aligners/aligner.js
+++ static/script/widgets/carousel/aligners/aligner.js
@@ -22,14 +22,14 @@
  * Please contact us for an alternative licence
  */
 require.def(
-    'antie/widgets/carousel/aligners/aligner',
-    [
+    'antie/widgets/carousel/aligners/aligner', [
         'antie/class',
         'antie/events/beforealignevent',
         'antie/events/afteralignevent',
-        'antie/widgets/carousel/aligners/alignmentqueue'
+        'antie/widgets/carousel/aligners/alignmentqueue',
+        'antie/runtimecontext'
     ],
-    function (Class, BeforeAlignEvent, AfterAlignEvent, AlignmentQueue) {
+    function(Class, BeforeAlignEvent, AfterAlignEvent, AlignmentQueue, RuntimeContext) {
         'use strict';
         /**
          * Converts simple index based alignment instructions to combinations of
@@ -40,8 +40,8 @@ require.def(
          * @param {antie.widgets.carousel.Mask} mask The carousel's mask object
          */
         var Aligner;
-        Aligner = Class.extend(/** @lends antie.widgets.carousel.aligners.Aligner.prototype */{
-            init: function (mask) {
+        Aligner = Class.extend( /** @lends antie.widgets.carousel.aligners.Aligner.prototype */ {
+            init: function(mask) {
                 this._mask = mask;
                 this._queue = new AlignmentQueue(this._mask);
                 this._lastAlignIndex = null;
@@ -60,7 +60,7 @@ require.def(
              * @param {Boolean} [options.skipAnim] If set true, the alignment will complete instantly then fire any provided callback
              * @param {Function} [options.onComplete] A function which will be executed on completion of the alignment animation.
              */
-            alignNext: function (navigator, options) {
+            alignNext: function(navigator, options) {
                 this._align(navigator, Aligner.directions.FORWARD, options);
             },
 
@@ -77,7 +77,7 @@ require.def(
              * @param {Boolean} [options.skipAnim] If set true, the alignment will complete instantly then fire any provided callback
              * @param {Function} [options.onComplete] A function which will be executed on completion of the alignment animation.
              */
-            alignPrevious: function (navigator, options) {
+            alignPrevious: function(navigator, options) {
                 this._align(navigator, Aligner.directions.BACKWARD, options);
             },
 
@@ -94,7 +94,7 @@ require.def(
              * @param {Boolean} [options.skipAnim] If set true, the alignment will complete instantly then fire any provided callback
              * @param {Function} [options.onComplete] A function which will be executed on completion of the alignment animation.
              */
-            alignToIndex: function (index, options) {
+            alignToIndex: function(index, options) {
                 this._informMaskBeforeAlign(index);
                 this._moveNormally(index, options);
             },
@@ -103,7 +103,7 @@ require.def(
              * Get the last index the carousel was asked to align to
              * @returns {Number} The last index the carousel was asked to align to, or null if no alignments have completed.
              */
-            indexOfLastAlignRequest: function () {
+            indexOfLastAlignRequest: function() {
                 return this._lastAlignIndex;
             },
 
@@ -111,11 +111,11 @@ require.def(
              * Instantly completes any in-flight alignment animations, firing any callbacks that were provided.
              * If several alignments have been queued, all will complete in order.
              */
-            complete: function () {
+            complete: function() {
                 this._queue.complete();
             },
 
-            _align: function (navigator, direction, options) {
+            _align: function(navigator, direction, options) {
                 var startIndex, targetIndex;
 
                 startIndex = this.indexOfLastAlignRequest();
@@ -126,12 +126,38 @@ require.def(
                     if (this._isWrap(startIndex, targetIndex, direction)) {
                         this._wrap(startIndex, targetIndex, navigator, direction, options);
                     } else {
-                        this._moveNormally(targetIndex, options);
+                        var widget = this._mask.getWidgetStrip();
+                        if (targetIndex === 1) {
+                            this._animation = RuntimeContext.getDevice().moveElementTo({
+                                el: widget.outputElement,
+                                to: {
+                                    left: 0
+                                },
+                                duration: options.duraton,
+                                easing: options.easing,
+                                skipAnim: true
+                            })
+                        } else if (targetIndex === 10) {
+                            // this._animation = RuntimeContext.getDevice().moveElementTo({
+                            //     el: widget.outputElement,
+                            //     to: {
+                            //         left: parseInt(widget.outputElement.style.left.replace(/px/g, '')) - 42 + 'px'
+                            //     },
+                            //     duration: options.duraton,
+                            //     easing: options.easing,
+                            //     skipAnim: true
+                            // })
+                        } else if (targetIndex > 1 && targetIndex < 10) {
+                            // if (this._animation) {
+                            //     RuntimeContext.getDevice().stopAnimation(this._animation);
+                            // }
+                            this._moveNormally(targetIndex, options);
+                        }
                     }
                 }
             },
 
-            _subsequentIndexInDirection: function (navigator, direction) {
+            _subsequentIndexInDirection: function(navigator, direction) {
                 var startPoint, lastAligned;
                 lastAligned = this.indexOfLastAlignRequest();
                 startPoint = (lastAligned === null) ? 0 : lastAligned;
@@ -142,7 +168,7 @@ require.def(
                 }
             },
 
-            _isWrap: function (startIndex, targetIndex, direction) {
+            _isWrap: function(startIndex, targetIndex, direction) {
                 if (direction === Aligner.directions.FORWARD && startIndex > targetIndex) {
                     return true;
                 }
@@ -152,16 +178,16 @@ require.def(
                 return false;
             },
 
-            _informMaskBeforeAlign: function (index) {
+            _informMaskBeforeAlign: function(index) {
                 this._mask.beforeAlignTo(this._lastAlignIndex, index);
                 this._lastAlignIndex = index;
             },
 
-            _informMaskAfterAlign: function (index) {
+            _informMaskAfterAlign: function(index) {
                 this._mask.afterAlignTo(index);
             },
 
-            _wrap: function (fromIndex, toIndex, navigator, direction, options) {
+            _wrap: function(fromIndex, toIndex, navigator, direction, options) {
                 if (this._fromIndexActive(fromIndex, navigator)) {
                     this._visibleActiveItemWrap(fromIndex, toIndex, navigator, direction, options);
                 } else {
@@ -169,20 +195,20 @@ require.def(
                 }
             },
 
-            _fromIndexActive: function (fromIndex, navigator) {
+            _fromIndexActive: function(fromIndex, navigator) {
                 var activeIndex;
                 activeIndex = navigator.currentIndex();
                 return fromIndex === activeIndex;
             },
 
-            _invisibleActiveItemWrap: function (fromIndex, toIndex, navigator, direction, options) {
+            _invisibleActiveItemWrap: function(fromIndex, toIndex, navigator, direction, options) {
                 var index = this._firstIndexToAlignForInvisibleActiveItemWrap(fromIndex, navigator, direction);
-                this._queue.add(index, {skipAnim: true});
+                this._queue.add(index, { skipAnim: true });
                 this._queueFinalAlign(toIndex, options);
                 this._queue.start();
             },
 
-            _firstIndexToAlignForInvisibleActiveItemWrap: function (fromIndex, navigator, direction) {
+            _firstIndexToAlignForInvisibleActiveItemWrap: function(fromIndex, navigator, direction) {
                 var widgetCount;
                 widgetCount = navigator.indexCount();
                 if (direction === Aligner.directions.FORWARD) {
@@ -192,7 +218,7 @@ require.def(
                 }
             },
 
-            _visibleActiveItemWrap: function (fromIndex, toIndex, navigator, direction, options) {
+            _visibleActiveItemWrap: function(fromIndex, toIndex, navigator, direction, options) {
                 var firstAlignIndex;
                 firstAlignIndex = this._firstIndexToAlignForVisibleActiveItemWrap(toIndex, navigator, direction);
                 this._queue.add(firstAlignIndex, options);
@@ -200,7 +226,7 @@ require.def(
                 this._queue.start();
             },
 
-            _firstIndexToAlignForVisibleActiveItemWrap: function (toIndex, navigator, direction) {
+            _firstIndexToAlignForVisibleActiveItemWrap: function(toIndex, navigator, direction) {
                 if (direction === Aligner.directions.FORWARD) {
                     return navigator.indexCount() + toIndex;
                 } else {
@@ -208,12 +234,12 @@ require.def(
                 }
             },
 
-            _moveNormally: function (toIndex, options) {
+            _moveNormally: function(toIndex, options) {
                 this._queueFinalAlign(toIndex, options);
                 this._queue.start();
             },
 
-            _queueFinalAlign: function (toIndex,  options) {
+            _queueFinalAlign: function(toIndex, options) {
                 var optionsWithCallback, self;
 
                 function OptionsClone() {}
