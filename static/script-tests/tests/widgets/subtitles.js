d6f12bc92c22456310dca9beb28765f262482ba2
postmaterialist <postmaterialist@users.noreply.github.com>
2016-09-08 14:47:41 +0100

    add basic subtitles widget

---
+++ static/script-tests/tests/widgets/subtitles.js
@@ -0,0 +1,259 @@
+require(
+    [
+        'antie/application',
+        'antie/widgets/subtitles',
+        'antie/widgets/widget',
+        'antie/devices/browserdevice',
+        'antie/widgets/container',
+        'antie/widgets/label',
+        'antie/devices/mediaplayer/mediaplayer',
+        'antie/subtitles/timedtext'
+    ],
+    function(Application, Subtitles, Widget, BrowserDevice, Container, Label, MediaPlayer, TimedText) {
+        'use strict';
+
+        describe('antie.widgets.Subtitles', function() {
+            var mockGetMediaTimeCallback;
+            var mockBrowserDevice;
+            var mockContainer;
+            var mockApplication;
+            var mockTimedText;
+            var mockOutputElement;
+            var mockLabel;
+
+            var mockTimedTextElement1, mockTimedTextElement2, mockTimedTextElement3;
+            var mockActiveElements;
+
+            beforeEach(function () {
+                mockGetMediaTimeCallback = function(){
+                    return 100.1;
+                };
+                mockTimedText = Object.create(TimedText.prototype);
+                mockTimedText.getActiveElements = function(){};
+                mockBrowserDevice = Object.create(BrowserDevice.prototype);
+                mockApplication = Object.create(Application.prototype);
+                mockContainer = Object.create(Container.prototype);
+                mockOutputElement = Object.create(Container.prototype);
+                mockLabel = Object.create(Label.prototype);
+
+                mockTimedTextElement1 = Object.create(TimedText.prototype);
+                mockTimedTextElement1.getTextContent = function(){}; //TODO use the proper function here
+                spyOn(mockTimedTextElement1, 'getTextContent').andReturn('textContent1');
+                mockTimedTextElement2 = Object.create(TimedText.prototype);
+                mockTimedTextElement2.getTextContent = function(){}; //TODO use the proper function here
+                spyOn(mockTimedTextElement2, 'getTextContent').andReturn('textContent2');
+                mockTimedTextElement3 = Object.create(TimedText.prototype);
+                mockTimedTextElement3.getTextContent = function(){}; //TODO use the proper function here
+                spyOn(mockTimedTextElement3, 'getTextContent').andReturn('textContent3');
+
+                spyOn(mockBrowserDevice, 'createContainer').andReturn(mockContainer);
+                spyOn(mockBrowserDevice, 'createLabel').andReturn(mockLabel);
+                spyOn(mockBrowserDevice, 'showElement');
+                spyOn(mockBrowserDevice, 'hideElement');
+                spyOn(mockBrowserDevice, 'clearElement');
+                spyOn(mockBrowserDevice, 'appendChildElement');
+
+                mockActiveElements = [mockTimedTextElement1, mockTimedTextElement2, mockTimedTextElement3];
+
+                spyOn(mockTimedText, 'getActiveElements').andReturn(mockActiveElements);
+                spyOn(mockApplication, 'getDevice').andReturn(mockBrowserDevice);
+
+                spyOn(Widget.prototype, 'addClass');
+                spyOn(Widget.prototype, 'getCurrentApplication').andReturn(mockApplication);
+            });
+
+            afterEach(function() {
+            });
+
+            it('can be constructed', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+
+                expect(subtitles._getMediaTimeCallback).toBe(mockGetMediaTimeCallback);
+                expect(subtitles._timedText).toBe(mockTimedText);
+
+                expect(Widget.prototype.addClass).toHaveBeenCalledWith('subtitles');
+            });
+
+            it('will render a new outputDevice if one doesnt exist', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                var outputElement = subtitles.render(mockBrowserDevice);
+
+                expect(mockBrowserDevice.createContainer).toHaveBeenCalledWith('id');
+                expect(outputElement).toBe(mockContainer);
+            });
+
+            it('wont render a new outputDevice if one exists already', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+
+                var secondMockContainer = Object.create(Container.prototype);
+                subtitles.outputElement = secondMockContainer;
+
+                var outputElement = subtitles.render(mockBrowserDevice);
+
+                expect(mockBrowserDevice.createContainer).not.toHaveBeenCalled();
+                expect(outputElement).toBe(secondMockContainer);
+            });
+
+            it('can display subtitles if the widget is rendered', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                var outputElement = subtitles.render(mockBrowserDevice);
+
+                subtitles.start();
+
+                expect(mockBrowserDevice.showElement).toHaveBeenCalledWith(
+                    {
+                        el: outputElement,
+                        skipAnim: true
+                    }
+                );
+            });
+
+            it('wont show subtitles if the widget is not rendered', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+
+                subtitles.start();
+
+                expect(mockBrowserDevice.showElement).not.toHaveBeenCalled();
+            });
+
+            it('will set an interval to call the update function once start is called', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                spyOn(window, 'setInterval').andReturn(1);
+                spyOn(subtitles, 'update');
+
+                subtitles.start();
+
+                expect(subtitles._updateInterval).toBe(1);
+                expect(window.setInterval).toHaveBeenCalledWith(jasmine.any(Function), 750);
+
+                // check the timeout is set to call the update function
+                expect(subtitles.update).not.toHaveBeenCalled();
+
+                //call it
+                window.setInterval.calls[0].args[0]();
+
+                expect(subtitles.update).toHaveBeenCalled();
+            });
+
+            it('will not set a new interval if there is one already in place', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                spyOn(window, 'setInterval').andReturn(1);
+                spyOn(subtitles, 'update');
+                subtitles._updateInterval = 2;
+
+                subtitles.start();
+
+                expect(subtitles._updateInterval).toBe(2);
+                expect(window.setInterval).not.toHaveBeenCalled();
+            });
+
+            it('can stop displaying subtitles if the widget is rendered', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                var outputElement = subtitles.render(mockBrowserDevice);
+
+                subtitles.stop();
+
+                expect(mockBrowserDevice.hideElement).toHaveBeenCalledWith(
+                    {
+                        el: outputElement,
+                        skipAnim: true
+                    }
+                );
+            });
+
+            it('wont hide subtitles if the widget is not rendered', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+
+                subtitles.stop();
+
+                expect(mockBrowserDevice.hideElement).not.toHaveBeenCalled();
+            });
+
+            it('will clear the update timeout if it is set', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                subtitles._updateInterval = 10;
+
+                subtitles.stop();
+
+                expect(subtitles._updateInterval).toEqual(null);
+            });
+
+            it('update function will stop the subtitles widget if there is no media player set', function() {
+                var subtitles = new Subtitles('id', mockTimedText, null);
+                spyOn(subtitles, 'stop');
+
+                subtitles.update();
+
+                expect(subtitles.stop).toHaveBeenCalled();
+            });
+
+            it('update function will call updateCaptions with the time from the _getMediaTimeCallback', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                spyOn(subtitles, 'stop');
+                spyOn(subtitles, '_updateCaptions');
+
+                subtitles.update();
+
+                expect(subtitles.stop).not.toHaveBeenCalled();
+                expect(subtitles._updateCaptions).toHaveBeenCalledWith(100.1);
+            });
+
+
+            it('_updateCaptions will update the activeElements if they are different and redraw the widget contents', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                spyOn(subtitles, '_removeCaptions');
+                spyOn(subtitles, '_addCaptions');
+
+                expect(subtitles._activeElements).toEqual([]);
+
+                subtitles._updateCaptions(300);
+
+                expect(mockTimedText.getActiveElements).toHaveBeenCalledWith(300);
+                expect(subtitles._activeElements).toEqual(mockActiveElements);
+                expect(subtitles._addCaptions).toHaveBeenCalledWith(mockActiveElements);
+                expect(subtitles._removeCaptions).toHaveBeenCalled();
+            });
+
+            it('_updateCaptions will NOT update the activeElements if they are NOT different and redraw the widget contents', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                spyOn(subtitles, '_removeCaptions');
+                spyOn(subtitles, '_addCaptions');
+
+                subtitles._activeElements = mockActiveElements;
+
+                expect(subtitles._activeElements).toEqual(mockActiveElements);
+
+                subtitles._updateCaptions(300);
+
+                expect(mockTimedText.getActiveElements).toHaveBeenCalledWith(300);
+                expect(subtitles._activeElements).toEqual(mockActiveElements);
+                expect(subtitles._addCaptions).not.toHaveBeenCalled();
+                expect(subtitles._removeCaptions).not.toHaveBeenCalled();
+            });
+
+
+            it('_removeCaptions will clear the outputElement', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                subtitles.outputElement = mockOutputElement;
+
+                subtitles._removeCaptions();
+
+                expect(mockBrowserDevice.clearElement).toHaveBeenCalledWith(mockOutputElement);
+            });
+
+            it('_addCaptions will add create a label for each active element and append it to the outputElement', function() {
+                var subtitles = new Subtitles('id', mockTimedText, mockGetMediaTimeCallback);
+                subtitles.outputElement = mockOutputElement;
+
+                subtitles._addCaptions(mockActiveElements);
+
+                expect(mockBrowserDevice.appendChildElement.calls.length).toBe(3);
+                expect(mockBrowserDevice.appendChildElement).toHaveBeenCalledWith(mockOutputElement, mockLabel);
+
+                expect(mockBrowserDevice.createLabel).toHaveBeenCalledWith('id', ['subtitlesLabel'], 'textContent1');
+                expect(mockBrowserDevice.createLabel).toHaveBeenCalledWith('id', ['subtitlesLabel'], 'textContent2');
+                expect(mockBrowserDevice.createLabel).toHaveBeenCalledWith('id', ['subtitlesLabel'], 'textContent3');
+            });
+        });
+    }
+);
---
+++ static/script/widgets/subtitles.js
@@ -0,0 +1,162 @@
+
+/**
+ * @fileOverview Requirejs module containing the antie.widgets.Subtitles class.
+ *
+ * @preserve Copyright (c) 2013 British Broadcasting Corporation
+ * (http://www.bbc.co.uk) and TAL Contributors (1)
+ *
+ * (1) TAL Contributors are listed in the AUTHORS file and at
+ *     https://github.com/fmtvp/TAL/AUTHORS - please extend this file,
+ *     not this notice.
+ *
+ * @license Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * All rights reserved
+ * Please contact us for an alternative licence
+ */
+
+define(
+    'antie/widgets/subtitles',
+    ['antie/widgets/widget'],
+    function(Widget) {
+        'use strict';
+
+        /**
+         *
+         * @name antie.widgets.Subtitles
+         * @class
+         * @extends antie.widgets.Widget
+         * @param {String} [id] The unique ID of the widget. If excluded, a temporary internal ID will be used (but not included in any output).
+         * @param {antie.subtitles.TimedText} media Media widget which will be playing the content that the captions are associated with.
+         * @param {antie.widgets.Media} media Media widget which will be playing the content that the captions are associated with.
+        */
+        var Subtitles = Widget.extend(/** @lends antie.widgets.Subtitles.prototype */ {
+            /**
+             * @constructor
+             * @ignore
+             */
+            init: function(id, timedText, getMediaTimeCallback) {
+                this._super(id);
+
+                this._timedText = timedText;
+                this._getMediaTimeCallback = getMediaTimeCallback;
+                this._activeElements = [];
+
+                this.addClass('subtitles');
+            },
+            /**
+             * Renders the widget and any child widgets to device-specific output.
+             * @param {antie.devices.Device} device The device to render to.
+             * @returns A device-specific object that represents the widget as displayed on the device (in a browser, a DOMElement);
+             */
+            render: function(device) {
+                if (!this.outputElement) {
+                    this.outputElement = device.createContainer(this.id);
+                }
+                return this.outputElement;
+            },
+
+            /**
+             * Starts displaying the captions.
+             */
+            start: function () {
+                if(!this._updateInterval){
+                    this._updateInterval = setInterval(this.update.bind(this), 750);
+                }
+
+                if (this.outputElement) {
+                    var device = this.getCurrentApplication().getDevice();
+                    device.showElement({
+                        el: this.outputElement,
+                        skipAnim: true
+                    });
+                }
+            },
+            /**
+             * Stops displaying the captions.
+             */
+            stop: function () {
+                if(this._updateInterval){
+                    clearInterval(this._updateInterval);
+                    this._updateInterval = null;
+                }
+
+                if (this.outputElement) {
+                    var device = this.getCurrentApplication().getDevice();
+                    device.hideElement({
+                        el: this.outputElement,
+                        skipAnim: true
+                    });
+                }
+                // this._cleanOldCaptions(this._media.getDuration());
+            },
+
+            /**
+             * Update the subtitles on screen with those that are current
+             * according to the time of the mediaPlayer
+             */
+            update: function(){
+                if (!this._getMediaTimeCallback) {
+                    // clean up
+                    this.stop();
+                } else {
+                    var time = this._getMediaTimeCallback();
+                    this._updateCaptions(time);
+                }
+            },
+
+            /**
+             * Update captions in the DOM for elements at a given time.
+             * @private
+             * @param {Integer} time The time (in seconds) to show the captions for.
+             */
+            _updateCaptions: function (time) {
+                var currentActiveElements = this._timedText.getActiveElements(time);
+
+                if(currentActiveElements !== this._activeElements){
+                    this._activeElements = currentActiveElements;
+                    // // Clear out old captions
+                    this._removeCaptions();
+                    // Add new captions
+                    this._addCaptions(this._activeElements);
+                }
+            },
+
+            /**
+             * Update captions in the DOM for elements at a given time.
+             * @private
+             * @param {Integer} time The time (in seconds) to show the captions for.
+             */
+            _removeCaptions: function () {
+                var device = this.getCurrentApplication().getDevice();
+                device.clearElement(this.outputElement);
+            },
+
+            /**
+             * Update captions in the DOM for elements at a given time.
+             * @private
+             * @param {Array} [antie.subtitles.TimedTextElement] activeElements
+             *                  the array of active timedTextElements to display.
+             */
+            _addCaptions: function (activeElements) {
+                var device = this.getCurrentApplication().getDevice();
+
+                for(var i = 0; i < activeElements.length; i++){
+                    device.appendChildElement(this.outputElement, device.createLabel('id', ['subtitlesLabel'], activeElements[i].getTextContent()));
+                }
+            }
+        });
+
+        return Subtitles;
+    }
+);
